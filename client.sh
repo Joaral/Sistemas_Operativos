f5e50318a5ca1471307af2c380dc5d03926c7d2b
Joan <joan.ariza@enti.cat>
2024-11-14 10:53:40 +0100

    ejercicio en casa, protocolo update

--- client.sh
+++ client.sh
@@ -3,7 +3,7 @@ PORT="2022"
 echo "Cliente de Dragón magia Abuelita Miedo 2022"
 echo "1. ENVÍO DE CABECERA"
 
-echo "DMMM" | nc 127.0.0.1 $PORT
+echo "DMAM" | nc 127.0.0.1 $PORT
 DATA=`nc -l $PORT`
 # SI DATA ES DIFERENTE A OK_HEADER, MENSAJE
 # DE ERROR Y EXIT 1
@@ -11,4 +11,12 @@ if [ "$DATA" != "OK_HEADER" ]
 then
 	echo "ERROR 1: el header  se envio incorrectamente"
 	exit 1
+fi
+echo "3. ENVIO DE ARCHIVO"
+echo "FILE_NAME dragon.txt" | nc localhost $PORT
+DATA=`nc -l $PORT`
+if [ "$DATA" != "OK_FILE_NAME" ]
+then 
+	echo "ERROR 2: el archivo se envió incorrectamente"
+	exit 2
 fi
--- server.sh
+++ server.sh
@@ -5,13 +5,24 @@ echo "Servidor de Dragón Magia Abuelita Miedo 2022"
 echo "0.ESCUCHAMOS"
 
 DATA=`nc -l $PORT`
+
 if [ "$DATA" != "DMAM" ]
 then
 	echo "ERROR 1: Cabecera Incorrecta"
-	echo "KO_HEADER" | nc localhost 2022
+	echo "KO_HEADER" | nc localhost $PORT
 	exit 1
 fi
-
 echo "2. CHECK OK - Enviando OK_HEADER"
-echo "OK_HEADER" | nc localhost 2022
+echo "OK_HEADER" | nc localhost $PORT
+
 DATA=`nc -l $PORT`
+FILE_NAME=`echo "$DATA" | cut -d ' ' -f 1`
+
+if [ "$FILE_NAME" != "FILE_NAME" ]
+then
+	echo "ERROR 2: Archivo Incorrecto"
+	echo "KO_FILE_NAME" | nc localhost $PORT
+	exit 2
+fi 
+echo "4. CHECK OK FILE_NAME - Enviando OK_FILE_NAME"
+echo "OK_FILE_NAME" | nc localhost $PORT
